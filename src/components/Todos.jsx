73508b92ab9e90b35b398df614b694833a55b35a
Your Name <oghenetegafrancis@gmail.com>
2024-06-20 17:13:59 +0100

    add edit button

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,41 +1,97 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import {removeTodo} from '../features/todo/todoSlice'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTodo, editTodo } from '../features/todo/todoSlice';
 
-function Todos() {
+const Todos = () => {
+    const todos = useSelector(state => state.todos);
+    const dispatch = useDispatch();
 
-    const todos = useSelector(state => state.todos)
-    const dispatch = useDispatch()
+    const handleInputChange = (id, text) => {
+        dispatch(editTodo({ id, text }));
+    };
+
+    const handleEditClick = (id) => {
+        dispatch(editTodo({ id, isEditing: true }));
+    };
+
+    const handleSaveClick = (id, text) => {
+        dispatch(editTodo({ id, text, isEditing: false }));
+    };
 
     return (
-        <>
+        <div>
             <div>Todos</div>
             <ul className='list-none'>
-                {todos.map((todo) => 
-                <li
-                className='mt-4 flex justify-between items-center bg-zinc-700 px-4 py-2 rounded'
-                key={todo.id}
-                >
-                    <div className='text-white'>{todo.text} </div>
-                    <button 
-                    onClick={() => dispatch(removeTodo(todo.id))}
-                    className='text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md'>
-                        <svg 
-                        xmlns='https://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth={1.5}
-                        stroke='currentColor'
-                        className='w-6 h-6'
-                        >
-                        </svg>
-                    </button>
-                </li>
-                )}
+                {todos.map((todo) => (
+                    <li
+                    className='mt-4 flex justify-between items-center bg-zinc-700 px-4 py-2 rounded'
+                    key={todo.id}
+                    >
+                    {todo.isEditing ? (
+                        <input
+                            type='text'
+                            value={todo.text}
+                            onChange={(e) => handleInputChange(todo.id, e.target.value)}
+                            autoFocus
+                            className='bg-transparent text-white focus:outline-none'
+                            />
+                        ) : (
+                            <div className='text-white'>{todo.text}</div>
+                        )}
+                        <div className='flex space-x-3 py-2'>
+                            {todo.isEditing ? (
+                                <button
+                                    onClick={() => handleSaveClick(todo.id, todo.text)}
+                                    className='text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 rounded text-md'
+                                >
+                                    Save
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => handleEditClick(todo.id)}
+                                    className='text-white bg-blue-400 border-0 py-1 px-4 focus:outline-none hover:bg-blue-500 rounded text-md'
+                                >
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        fill='none'
+                                        viewBox='0 0 24 24'
+                                        strokeWidth={1.5}
+                                        stroke='currentColor'
+                                        className='w-6 h-6'
+                                    >
+                                        <path
+                                            strokeLinecap='round'
+                                            strokeLinejoin='round'
+                                            d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
+                                        />
+                                    </svg>
+                                </button>
+                            )}
+                            <button
+                                onClick={() => dispatch(removeTodo(todo.id))}
+                                className='text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md'
+                            >
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth={1.5}
+                                    stroke='currentColor'
+                                    className='w-6 h-6'
+                                >
+                                    <path
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                        d='M6 18L18 6M6 6l12 12'
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                ))}
             </ul>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default Todos
+export default Todos;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit"
 
 const initialState = {
-    todos: [{id: 1, text: 'Hello'}],
+    todos: [{id: "1", text: 'Hello', isEditing: false}],
 
 }
 
@@ -13,16 +13,29 @@ export const todoSlice = createSlice({
             const todo = {
                 id: nanoid(),
                 text: action.payload,
+                isEditing: false,
             }
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) =>
             todo.id !== action.payload)
+        },
+        editTodo: (state, action) => {
+            const { id, text, isEditing } = action.payload
+            const todoIndex = state.todos.findIndex((todo) => todo.id === id)
+            if (todoIndex !== -1) {
+                if (text !== undefined) {
+                    state.todos[todoIndex].text = text;
+                }
+                if (isEditing !== undefined) {
+                    state.todos[todoIndex].isEditing = isEditing;
+                }
+            }
         }
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, editTodo} = todoSlice.actions
 
 export default todoSlice.reducer
